0e7f939926272746c6ca22356b19b6509c0ddc0f
JulianDAGUZ <juliandaguzz@gmail.com>
2023-12-28 16:25:39 -0500

    Update

--- Spring-Security-Course/src/main/java/com/cursos/api/SpringSecurityCourse/persistence/repository/ProductRepository.java
+++
@@ -1,7 +0,0 @@
-package com.cursos.api.SpringSecurityCourse.persistence.repository;
-
-import com.cursos.api.SpringSecurityCourse.persistence.entity.Product;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface ProductRepository extends JpaRepository<Product, Long> {
-}
