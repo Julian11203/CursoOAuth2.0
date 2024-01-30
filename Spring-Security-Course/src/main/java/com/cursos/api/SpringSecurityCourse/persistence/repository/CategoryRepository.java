803c7fa5b9a1332d6d431aa4bfd00cb3e316a8ae
Julian David Guzman Caballero <jguzmanc@agrosavia.co>
2024-01-30 13:12:20 -0500

    Update

--- Spring-Security-Course/src/main/java/com/cursos/api/SpringSecurityCourse/persistence/repository/CategoryRepository.java
+++
@@ -1,7 +0,0 @@
-package com.cursos.api.SpringSecurityCourse.persistence.repository;
-
-import com.cursos.api.SpringSecurityCourse.persistence.entity.Category;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface CategoryRepository extends JpaRepository<Category, Long> {
-}
--- Spring-Security-Course/src/main/java/com/cursos/api/SpringSecurityCourse/service/RoleService.java
+++
@@ -1,9 +0,0 @@
-package com.cursos.api.SpringSecurityCourse.service;
-
-import com.cursos.api.SpringSecurityCourse.persistence.entity.security.Role;
-
-import java.util.Optional;
-
-public interface RoleService {
-    Optional<Role> findDefaultRole();
-}
